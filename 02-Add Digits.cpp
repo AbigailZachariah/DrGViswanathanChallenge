4ed7933e633a39a66903bdde6573664834cd2768
Abigail Zachariah <abigail2005zachariah@gmail.com>
2026-06-06 23:36:08 +0300

    Add Digits

---
+++ 02-Add Digits.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int addDigits(int num) {
+
+    if(num/10==0){
+        return num;
+    }
+
+    int sum=0;
+    while(num>0){
+        int r=num%10;
+        sum+=r;
+        num/=10;
+    }
+    return addDigits(sum);
+}
+
+int main(){
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+
+    cout<<addDigits(n);
+    return 0;
+}
